210dedbf4e33d2aa4d65cc4ca3f77de1f5169fb2
TheMichelangelo <TheMichelangelo@users.noreply.github.com>
2019-12-09 21:36:06 +0200

    Update musician.rb

--- app/models/musician.rb
+++ app/models/musician.rb
@@ -1,8 +1,8 @@
 class Musician < ApplicationRecord
-  validates :home_adress , :length => {:maximum => 50},:presence => {:message => "Вкажіть "}
-  validates :name,:presence => {:message => "Вкажіть "},uniqueness: true
-  validates :home_adress,:presence => {:message => "Вкажіть "},uniqueness: true
-  validates :surname, :presence => {:message => "Вкажіть "}
+  validates :home_adress , :length => {:maximum => 50}
+  validates :name,:presence => {:message => "Вкажіть ім'я музиканта"},uniqueness: true
+  validates :home_adress,:presence => {:message => "Вкажіть домашню адресу виконавця"},uniqueness: true
+  validates :surname, :presence => {:message => "Вкажіть прізвище виконавця"}
   has_many :albums
   belongs_to :studio
 end
