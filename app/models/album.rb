4a4212688aeafd433127252df6807bac06f94dd0
TheMichelangelo <TheMichelangelo@users.noreply.github.com>
2019-12-09 21:35:04 +0200

    Update album.rb

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,7 +1,7 @@
 class Album < ApplicationRecord
   validates :name , :length => {:maximum => 72}, :presence => {:message => "Вкажіть ім'я"}
-  validates :price, :presence => {:message => "Вкажіть "}
-  validates :type, :presence => {:message => "Вкажіть "}
-  validates :genre, :presence => {:message => "Вкажіть "}
+  validates :price, :presence => {:message => "Вкажіть ціну"}
+  validates :type, :presence => {:message => "Вкажіть тип"}
+  validates :genre, :presence => {:message => "Вкажіть жанр музики"}
   belongs_to :musician
 end
